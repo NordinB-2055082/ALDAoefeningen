cfd78cec89d0483479d0e89159c62e3645fa8706
unknown <nordin.ben-al-lal@student.uhasselt.be>
2023-11-28 09:01:36 +0100

    fout bij grote dataset bad memory alloc

--- Taak 2/testReadFIle/testReadFIle/ProductSearch.cpp
+++ Taak 2/testReadFIle/testReadFIle/ProductSearch.cpp
@@ -11,7 +11,9 @@ ProductSearch::~ProductSearch() {
     delete titleTrieRoot;
 }
 
-
+/*
+Reading a product and putting it into the vector is seen as O(1)
+*/
 void ProductSearch::loadProducts(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
